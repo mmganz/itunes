8f6521bdd1f3c7d5006ee59a6d2c573fbe508310
Meredith Ganz <2meredithganz@gmail.com>
2016-09-27 13:33:10 -0600

    push

---
+++ app.js
@@ -0,0 +1,14 @@
+function getMusic(){
+  var artist = document.getElementById('artist').value;
+  itunes.getMusicByArtist(artist).then(drawSongs);
+}
+
+function drawSongs(songList){
+  var songElem = document.getElementById('song-list')
+  for (var i = 0; i < songList.length; i++) {
+    var songs =songList[i];
+    songElem.innerHTML+= `<h1>${songs.title}</h1>`
+  }
+  console.log(songList);
+  
+}
