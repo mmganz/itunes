fa83fbda0e077e0cdd6b3de50c12a50e065434af
Meredith Ganz <2meredithganz@gmail.com>
2016-09-27 13:53:17 -0600

    add html

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ function getMusic(){
 
 function drawSongs(songList){
   var songElem = document.getElementById('song-list')
+  songElem.innerHTML = ''
   for (var i = 0; i < songList.length; i++) {
-    var songs =songList[i];
+    var songs =songList[i]
     songElem.innerHTML+= `<h1>${songs.title}</h1>`
   }
   console.log(songList);
