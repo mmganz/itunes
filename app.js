45c3fbede1f7eefb0f41904ed3d626e78ba5f7f6
Meredith Ganz <2meredithganz@gmail.com>
2016-10-06 19:49:24 -0600

    push

--- app.js
+++ app.js
@@ -49,7 +49,6 @@ function drawSongs(songList, target){
 if(target != "#my-tunes"){
       target = '#song-list'}
 
-//  function update(list, target){
    var elem = $(target)
    elem.empty()
 
@@ -88,7 +87,7 @@ if(target != "#my-tunes"){
             </audio>
              <button onclick="removeh(${songs.id})" class="btn btn-secondary">Remove Song</button>
               <button class="btn btn-secondary promote">Promote Song</button>
-               <button class="btn btn-secondary demote">Demote</button>
+               <button class="btn btn-secondary demote">Demote Song</button>
         </div>
       </div>
     </div>`
@@ -107,103 +106,8 @@ elem.append(target == '#song-list' ? songsTemplate : myTunes)
 }, 
 true);
 
-//  }//function update
+
 
 
 }//draw songs
-
-// drawSongs();
-
-
-// var myTunesElem = document.getElementById('my-tunes')
-// myTunesElem.innerHTML = ''
-// var template = ''
-// for (var i = 0; i < songList.length; i++) {
-//     var songs =songList[i]
-//     template+= ` 
-//       <h1>My Tunes</h1>    
-//     <div class="template">
-//       <div class="card">
-//          <img class="card-img-top" src="${songs.albumArt}">
-//           <div class="card-block">
-//           <h6 class="card-title title text">${songs.title}</h6>
-//           <p class="card-subtitle artist text">${songs.artist}</p>
-//           <p class="card-text album text">${songs.collection}</p>
-//           <p class="card-text price text">$${songs.price}</p>
-//           <p class="card-text id">${songs.id}</p>
-//           <audio controls>
-//             <source id="audios" src="${songs.preview}">
-//             </audio>
-//              <button id="${songs.id}" class="btn btn-secondary">Add Song to My Tunes</button>
-//         </div>
-//       </div>
-//     </div>`
-//   }
-
-//     document.addEventListener('play', function(e){
-//   var audios = document.getElementsByTagName('audio');
-//   for(var i = 0, len = audios.length; i < len;i++){
-//       if(audios[i] != e.target){
-//           audios[i].pause();
-//       }
-//   }
-// }, 
-// true);
-
-
-// //   document.addEventListener('play', function(e){
-// //   var audios = document.getElementsByTagName('audio');
-// //   for(var i = 0, len = audios.length; i < len;i++){
-// //       if(audios[i] != e.target){
-// //           audios[i].pause();
-// //       }
-// //   }
-// // }, 
-// // true);
-
-// //   myTunesElem.innerHTML = template;
-// //   console.log(songList);
-
-// // })
-
-
-
-
-//   var songElem = document.getElementById('song-list')
-//   songElem.innerHTML = ''
-//   var template = ''
-//   for (var i = 0; i < songList.length; i++) {
-//     var songs =songList[i]
-//     template+= ` <div class="template">
-//       <div class="card">
-//          <img class="card-img-top" src="${songs.albumArt}">
-//           <div class="card-block">
-//           <h6 class="card-title title text">${songs.title}</h6>
-//           <p class="card-subtitle artist text">${songs.artist}</p>
-//           <p class="card-text album text">${songs.collection}</p>
-//           <p class="card-text price text">$${songs.price}</p>
-//           <p class="card-text id">${songs.id}</p>
-//           <audio controls>
-//             <source id="audios" src="${songs.preview}">
-//             </audio>
-//              <button id="${songs.id}" class="btn btn-secondary">Add Song to My Tunes</button>
-//         </div>
-//       </div>
-//     </div>`
-//   }
-
-//   document.addEventListener('play', function(e){
-//   var audios = document.getElementsByTagName('audio');
-//   for(var i = 0, len = audios.length; i < len;i++){
-//       if(audios[i] != e.target){
-//           audios[i].pause();
-//       }
-//   }
-// }, 
-// true);
-
-//   songElem.innerHTML = template;
-//   console.log(songList);
-  
-// }
 
