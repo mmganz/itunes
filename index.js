80d81b9e6c8df3d00ad1ae681ae60fe384fc4722
Meredith Ganz <2meredithganz@gmail.com>
2016-10-28 11:03:42 -0600

    comit

---
+++ index.js
@@ -0,0 +1,20 @@
+let express = require('express'),
+  bodyParser = require('body-parser'),
+  cors = require('cors'),
+  routes = require('./server-assets/routes/index'),
+  handlers = require('./utils/handlers'),
+  server = express(),
+  port = process.env.PORT || 1582;
+  var http = require('http').Server(server)
+  Playlist = require('./server-assets/models/playlist-model');
+
+
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use('/', express.static(`${__dirname}/public/`));
+server.use('/', cors(handlers.corsOptions), routes.router)
+server.use('/', handlers.defaultErrorHandler)
+
+http.listen(port, function () {
+  console.log(`Running on: ${port}`);
+})
---
+++ server-assets/models/data-adapter.js
@@ -0,0 +1,15 @@
+let uuid = require('node-uuid'),
+  JsData = require('js-data'),
+  FBAdapter = require('js-data-firebase'),
+  DS = new JsData.DS();
+
+let fbAdapter = new FBAdapter({
+  basePath: 'https://my-musical-life.firebaseio.com/playlists'
+})
+
+DS.registerAdapter('firebase', fbAdapter, { default: true })
+
+module.exports = {
+  DS,
+  uuid
+}
